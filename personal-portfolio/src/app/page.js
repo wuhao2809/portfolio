3c2b5ed772cdec17e84f43b12f4faccd04de5eb8
wuhao2809 <wuhao2809@163.com>
2025-06-24 19:35:24 -0700

    finish about me

--- personal-portfolio/src/app/page.js
+++ personal-portfolio/src/app/page.js
@@ -1,9 +1,13 @@
 'use client'
 
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
-    <div><Hero/></div>
+    <div>
+      <Hero />
+      <About/>
+    </div>
   );
 }
